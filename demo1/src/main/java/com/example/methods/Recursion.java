69f11a8feac71ca7753a135efe13aa84c1e75a9d
sourabhmagadum <sourabhmagadum11@gmail.com>
2026-08-06 21:48:12 +0530

    recursion and pom.xml

--- demo1/src/main/java/com/example/methods/Recursion.java
+++ demo1/src/main/java/com/example/methods/Recursion.java
@@ -11,20 +11,34 @@ public class Recursion {
 	}
 	
 	public static void evenNum(int n, int a) {
-		System.out.println(n);
-		if(n%2 ==0 ) {
-			if(n<a) {
+//		System.out.println(n);
+		if(n%2 != 0 && n != a) {
+			System.out.println(n);
+			n++;
+			if(n == a)
 				return;
-			}
-			
 		}
+		evenNum(n++ ,a);
+	}
+	
+	public static int sumDigit(int num) {
 		
-		evenNum(n +1, a);
+		if(num == 0) 
+			return 0;
+		int d= num% 10;
+		num= num/10;
+		return d + sumDigit(num);
+	
+		
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		test(1);
-		evenNum(1,30);
+		evenNum(1,9);
+		
+//		System.out.println(sumDigit(244));
 	}
+	
+	
 
 }
